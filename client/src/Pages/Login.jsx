07a172cb0a2d41cc38d80f0f4dd5a7c9c434071f
Avinash <np6848586@gmail.com>
2025-12-27 20:21:56 +0530

    changes

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,4 +1,5 @@
-import { LoginForm } from "@/components/login-form"
+import { LoginForm } from "@/components/LoginForm";
+
 export default function Login() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
--- client/src/Pages/Signup.jsx
+++ client/src/Pages/Signup.jsx
@@ -1,5 +1,5 @@
-import { LoginForm } from "@/components/login-form"
-import { SignUpForm } from "@/components/Signup-form"
+import { SignUpForm } from "@/components/SignupForm";
+
 export default function Signup() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
